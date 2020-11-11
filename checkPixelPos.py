bd2e664acff828b8438762543aa2dc71d39c1817
Patryk Cebo <patrykcebo11@gmail.com>
2020-11-12 00:31:56 +0100

    save print

---
+++ checkPixelPos.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+print(pyautogui.size())
+
+pyautogui.moveTo(960, 750, duration = 1)
+pyautogui.click(960, 720)
--- square.py
+++ square.py
@@ -5,17 +5,22 @@ import time
 
 print(pyautogui.size())
 print("Podaj dlufosc boku w pixelach:")
-x: float =input()
+x =input()
 
 pyautogui.moveTo(10, 650, duration = 1)
 
 pyautogui.click(10, 650)
-time.sleep(5)
+time.sleep(2)
+#drawing a square
 pyautogui.moveTo(500, 300, duration = 1)
 pyautogui.dragRel(x, 0, duration = 1)
 pyautogui.dragRel(0, x, duration = 1)
 pyautogui.moveTo(500, 300, duration = 1)
 pyautogui.dragRel(0, x, duration = 1) #nie działa -x nie wiem dlaczego?
-pyautogui.dragRel(x, 0, duration = 1) 
-
+pyautogui.dragRel(x, 0, duration = 1)
+#saving a work of art
+pyautogui.hotkey("ctrlleft", "s")
+pyautogui.click(960, 720)
+print("Kwadrat o boku",x)
+#pyautogui.typewrite("Kwadrat o boku",x)
 
